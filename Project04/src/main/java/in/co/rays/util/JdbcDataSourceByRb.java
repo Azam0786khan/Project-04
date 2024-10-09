2589c544274ed30fcb99d4f270668d3e5a8e5c54
Azam khan <azamkhan5532@gmail.com>
2024-10-09 18:01:32 +0530

    Initial commit

---
+++ Project04/src/main/java/in/co/rays/util/JdbcDataSourceByRb.java
@@ -0,0 +1,79 @@
+package in.co.rays.util;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class JdbcDataSourceByRb {
+
+	private static JdbcDataSourceByRb jds = null;
+
+	private ComboPooledDataSource cpds = null;
+	
+	ResourceBundle rb = ResourceBundle.getBundle("in.co.rays.bundle.system");
+
+	private JdbcDataSourceByRb() {
+		try {
+			cpds = new ComboPooledDataSource();
+			cpds.setDriverClass(rb.getString("driver"));
+			cpds.setJdbcUrl(rb.getString("url"));
+			cpds.setUser(rb.getString("username"));
+			cpds.setPassword(rb.getString("password"));
+			cpds.setInitialPoolSize(Integer.parseInt(rb.getString("initialpoolsize")));
+			cpds.setAcquireIncrement(Integer.parseInt(rb.getString("acquireincrement")));
+			cpds.setMaxPoolSize(Integer.parseInt(rb.getString("maxpoolsize")));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static JdbcDataSourceByRb getInstance() {
+		if (jds == null) {
+			jds = new JdbcDataSourceByRb();
+		}
+		return jds;
+
+	}
+
+	public static Connection getConnection() {
+		try {
+			return getInstance().cpds.getConnection();
+
+		} catch (Exception e) {
+			return null;
+
+		}
+	}
+
+	public static void closeConnection(Connection conn, Statement stmt, ResultSet rs) {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void closeConnection(Connection conn, Statement stmt) {
+		closeConnection(conn, stmt, null);
+	}
+
+	public static void closeConnection(Connection conn) {
+		closeConnection(conn, null);
+
+	}
+
+}
+
